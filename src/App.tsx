1c1c4dd3e58e284866e557405a26eeb8b307d3da
Bruno Santos <brcesar19@gmail.com>
2024-01-10 19:01:06 -0300

    feat: eslint imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Button } from './components/ui/button';
 import './global.css';
+
+import { Button } from './components/ui/button';
 
 export function App() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex h-screen items-center justify-center">
       <h1>AAAA</h1>
-      <Button variant="destructive">Minhas Bola</Button>
+      <Button variant="destructive">Enviar</Button>
     </div>
   );
 }
